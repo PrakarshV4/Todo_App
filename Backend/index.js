93babb1d4088f86d24947db874e6b9768ac6e12b
Prakarsh Verma <prakarsh1248@gmail.com>
2024-02-09 10:40:07 +0530

    Added BE

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,23 @@
 const express = require ('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.post('/todo',function(req, res){
+
+    //input validation
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the incorrect inputs"
+        })
+        return
+    }
+
+    //Insert the data into MongoDb
+
 
 });
 
@@ -12,5 +26,11 @@ app.get('/todos',function(req, res){
 });
 
 app.put('/completed',function(req, res){
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the incorrect inputs"
+        })
+    }
 });
